0340505fd0d0980c37cec6116fae0d0ebeb8558d
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-03-05 16:47:02 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const API = "https://script.google.com/macros/s/AKfycbzsQybRdKpvftDfdASyhKEldTa_SKuteYeE7sNv3QOHaoKBoYBc4ek5RhMR1BqNwpBJ/exec";
+
+async function loadNextMatch(){
+
+const res = await fetch(API + "?action=nextmatch");
+
+const data = await res.json();
+
+document.getElementById("match").innerHTML =
+
+`<div class="card">
+
+${data.SUQADRA_CASA} vs ${data.SUQADRA_TRASFERTA}
+
+<br>
+
+${data.DATA} - ${data.ORA}
+
+</div>`;
+
+}
+
+loadNextMatch();
