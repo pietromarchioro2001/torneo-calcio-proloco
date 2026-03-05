bb98c8e9b00c02c94c429f8250aa6840d6ac03db
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-03-05 17:02:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const API = "https://script.google.com/macros/s/AKfycbzjY_WQLzAXOugLpyLJbdWWltAQkc4oPhRboPEvnd0xL1XBBL3bB80WjvROTyoPHhh6/exec";
+const API = "https://script.google.com/macros/s/AKfycbzW0P6NsswIahKqlw-h0d7p41ETqTkaaysoJ_nH-9_9hJCmYLUtSM6tqLA-z368_dpL/exec";
 
 async function loadNextMatch(){
 
 const res = await fetch(API + "?action=nextmatch");
 
 const data = await res.json();
+
+if(data.message){
+
+document.getElementById("match").innerHTML = `
+<div class="card">
+Nessuna partita programmata
+</div>
+`;
+
+return;
+}
 
 document.getElementById("match").innerHTML = `
 
