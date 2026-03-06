354327bc8cd4243774295c3571fed1e7850fa049
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-03-06 11:17:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const API = "https://script.google.com/macros/s/AKfycbzW0P6NsswIahKqlw-h0d7p41ETqTkaaysoJ_nH-9_9hJCmYLUtSM6tqLA-z368_dpL/exec";
 
+let teams = {};
+
+async function loadTeams(){
+
+const res = await fetch(API + "?action=teams");
+const data = await res.json();
+
+data.forEach(t=>{
+teams[t.TEAM_ID] = t.NOME_SQUADRA;
+});
+
+}
+
 async function loadNextMatch(){
 
 const res = await fetch(API + "?action=nextmatch");
-
 const data = await res.json();
 
-if(data.message){
-
-document.getElementById("match").innerHTML = `
-<div class="card">
-Nessuna partita programmata
-</div>
-`;
-
-return;
-}
+const home = teams[data.SUQADRA_CASA] || data.SUQADRA_CASA;
+const away = teams[data.SUQADRA_TRASFERTA] || data.SUQADRA_TRASFERTA;
 
 document.getElementById("match").innerHTML = `
 
 <div class="match">
 
-${data.SUQADRA_CASA} vs ${data.SUQADRA_TRASFERTA}
+${home} vs ${away}
 
 </div>
 
@@ -35,4 +39,11 @@ ${data.DATA} - ${data.ORA}
 
 }
 
-loadNextMatch();
+async function init(){
+
+await loadTeams();
+await loadNextMatch();
+
+}
+
+init();
