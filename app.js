86fc55ba9df07ed81a80a8faf1d9e320eb36eb2e
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-03-06 11:21:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,33 @@
 const API = "https://script.google.com/macros/s/AKfycbzW0P6NsswIahKqlw-h0d7p41ETqTkaaysoJ_nH-9_9hJCmYLUtSM6tqLA-z368_dpL/exec";
 
-let teams = {};
-
-async function loadTeams(){
-
-const res = await fetch(API + "?action=teams");
-const data = await res.json();
-
-data.forEach(t=>{
-teams[t.TEAM_ID] = t.NOME_SQUADRA;
-});
-
-}
-
 async function loadNextMatch(){
 
 const res = await fetch(API + "?action=nextmatch");
 const data = await res.json();
 
-const home = teams[data.SUQADRA_CASA] || data.SUQADRA_CASA;
-const away = teams[data.SUQADRA_TRASFERTA] || data.SUQADRA_TRASFERTA;
-
 document.getElementById("match").innerHTML = `
 
 <div class="match">
-
-${home} vs ${away}
-
+${data.SUQADRA_CASA} vs ${data.SUQADRA_TRASFERTA}
 </div>
 
 <div class="time">
-
-${data.DATA} - ${data.ORA}
-
+${formatDate(data.DATA)} - ${data.ORA}
 </div>
 
 `;
 
 }
 
-async function init(){
+function formatDate(date){
 
-await loadTeams();
-await loadNextMatch();
+const d = new Date(date);
+
+return d.toLocaleDateString("it-IT", {
+day:"numeric",
+month:"long"
+});
 
 }
 
-init();
+loadNextMatch();
