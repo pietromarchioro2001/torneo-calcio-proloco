d0934392d7eec4eb8611438ac6cd3c4c8ba03330
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-06-27 14:25:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ============================================================================
 const CONFIG = {
     // 🔥 SOSTITUISCI CON IL TUO URL APPS SCRIPT WEB APP
-    BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxky3w_r03m1C2MvJu1U3-5tbS55DP1BWILS7nyD6O3R0V_CGYu9XdEcuF3VMe0j_Jf/exec',
+    BACKEND_URL: 'https://script.google.com/macros/s/AKfycbw-sfcWsslk45yzexoUVDHYHVjYpCO-BSgpfuRpUhw2ufNF8xL4SsDyG7nTEYKv8loK/exec',
     API_TIMEOUT: 30000,
     CACHE_VERSION: 'v3.0',
     CACHE_MAX_AGE: 5 * 60 * 1000
